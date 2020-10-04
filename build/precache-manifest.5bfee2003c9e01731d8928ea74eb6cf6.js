53009a3a0656b6ed74d02baeee04ca068b6658c6
zhangqinglia <44956680+zhangqinglia@users.noreply.github.com>
2020-10-04 20:51:15 +0800

    Add files via upload
    66666

---
+++ build/precache-manifest.5bfee2003c9e01731d8928ea74eb6cf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6744e27e872c5e157650a414172303d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594f12994079e065fbbb",
+    "url": "/static/js/main.53e1b22d.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
